357cba678af35d4ae28c9489133637a5bee0433f
PasavitPanottrirat <p.pranottrirat@digipen.edu>
2022-07-15 20:26:42 -0700

    Speed clamp

--- Assets/Scripts/BouncerController.cs
+++ Assets/Scripts/BouncerController.cs
@@ -5,7 +5,10 @@ using TMPro;
 
 public class BouncerController : MonoBehaviour
 {
+    public int currentJump, currentSpeed;
     public int JumpHeight, MoveSpeed;
+    public float speedCap;
+    float moveDirection;
     public TextMeshProUGUI Indicator;
     Rigidbody rgbd;
     bool grounded;
@@ -14,6 +17,10 @@ public class BouncerController : MonoBehaviour
     void Start()
     {
         rgbd = GetComponent<Rigidbody>();
+
+        currentJump = Random.Range(1, 6);
+        currentSpeed = 6 - JumpHeight;
+
         JumpHeight = Random.Range(1, 6);
         MoveSpeed = 6 - JumpHeight;
     }
@@ -21,39 +28,53 @@ public class BouncerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Indicator.text = "Jump Height: " + JumpHeight + "\nMoveSpeed: " + MoveSpeed;
+        Indicator.text = "Jump Height: " + currentJump + "\nMove Speed: " + currentSpeed;
+
+    }
+
+    private void FixedUpdate()
+    {
         Movement();
-        //Jump();
 
         if (rgbd.velocity.y == 0.00f)
         {
             Jump();
         }
-
     }
 
     void Movement()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            rgbd.AddForce(Vector3.left * MoveSpeed * 10, ForceMode.Force);
+            rgbd.AddForce(Vector3.left * currentSpeed * 20, ForceMode.Force);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rgbd.AddForce(Vector3.right * MoveSpeed * 10, ForceMode.Force);
+            rgbd.AddForce(Vector3.right * currentSpeed * 20, ForceMode.Force);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            rgbd.AddForce(Vector3.down * MoveSpeed * 10, ForceMode.Force);
+            rgbd.AddForce(Vector3.down * MoveSpeed * 15, ForceMode.Force);
+        }
+
+        //Cap max speed
+        speedCap = currentSpeed * 10;
+        if (Mathf.Abs(rgbd.velocity.x) > speedCap)
+        {
+            Vector3 cappedX_velocity = Vector3.ClampMagnitude(rgbd.velocity, speedCap);
+            rgbd.velocity = new Vector3(cappedX_velocity.x, rgbd.velocity.y, rgbd.velocity.z);
         }
     }
 
     void Jump()
     {
-        float JumpForce = Mathf.Clamp(JumpHeight, 1f, 6f);
-        rgbd.AddForce(Vector3.up * JumpForce * 5f, ForceMode.Impulse);
+        currentJump = JumpHeight;
+        currentSpeed = MoveSpeed;
+
+        rgbd.AddForce(Vector3.up * JumpHeight * 5f, ForceMode.Impulse);
+
         JumpHeight = Random.Range(1, 6);
         MoveSpeed = 6 - JumpHeight;
     }
