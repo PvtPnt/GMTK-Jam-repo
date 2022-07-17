83ed6fd61a016e20587f34ea510cf0105b27eee9
Flairmin_O <ootayopas@hotmail.co.th>
2022-07-17 04:08:40 -0700

    player godmode fixed

--- Assets/Scripts/Player/BouncerController.cs
+++ Assets/Scripts/Player/BouncerController.cs
@@ -184,11 +184,11 @@ public class BouncerController : MonoBehaviour
         {
             resolveEnemyCollision(collision);
         }
-        else if (colTag == "Spike" && isGodMode < 0)
+        else if (colTag == "Spike" && isGodMode <= 0)
         {
             Respawn();
         }
-        else if(colTag == "Bullet" && isGodMode < 0)
+        else if(colTag == "Bullet" && isGodMode <= 0)
         {
             Destroy(collision.gameObject);
             Respawn();
@@ -226,7 +226,7 @@ public class BouncerController : MonoBehaviour
                 collision.gameObject.SetActive(false);
                 RandomStatus.SharedInstance.GetRandomStatus();
             }
-            else if(isGodMode < 0)
+            else if(isGodMode <= 0)
             {
                 Respawn();
             }
